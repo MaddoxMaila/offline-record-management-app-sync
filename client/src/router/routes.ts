16ccc8f4e1c50a5359900204220f778e38b92804
Tshepang Maila <tshepang.maila@fnb.co.za>
2025-01-16 21:47:52 +0200

    more routes for frontend

--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -1,31 +1,44 @@
 import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] =
-    [{
-        // path: '/',
-        // component: () => import('../layouts/MainLayout.vue'),
-        // children: [
-        //     {
-        //         name: 'Home',
-        //         path: '',
-        //         component: () => import('../views/SampleView.vue')
-        //     }
-        // ]
-        path: '/',
-        component: () => import('../layouts/AuthLayout.vue'),
-        children: [
-            {
-                name: 'Login',
-                path: '',
-                component: () => import('../views/auth/AuthView.vue')
-            },
-            {
-                name: 'Register',
-                path: 'register',
-                component: () => import('../views/auth/AuthView.vue')
-            },
-            
-        ]
-    }];
+    [
+        {
+            // path: '/',
+            // component: () => import('../layouts/MainLayout.vue'),
+            // children: [
+            //     {
+            //         name: 'Home',
+            //         path: '',
+            //         component: () => import('../views/SampleView.vue')
+            //     }
+            // ]
+            path: '/',
+            component: () => import('../layouts/AuthLayout.vue'),
+            children: [
+                {
+                    name: 'Login',
+                    path: '',
+                    component: () => import('../views/auth/AuthView.vue')
+                },
+                {
+                    name: 'Register',
+                    path: 'register',
+                    component: () => import('../views/auth/AuthView.vue')
+                },
+                
+            ]
+        },
+        {
+            path: '/home',
+            component: () => import('../layouts/MainLayout.vue'),
+            children: [
+                {
+                    name: 'Home',
+                    path: '',
+                    component: () => import('../views/HomeView.vue')
+                }
+            ]
+        }
+    ];
 
 export default routes;
